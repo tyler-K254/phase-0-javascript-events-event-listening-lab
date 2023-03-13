0fbc43cef15e071054e4c11c00388221b561a1b3
Tyler Kirumba <Tyler>
2023-03-13 11:26:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
+// document.querySelector("body").addEventListener("click", addingEventListener) 
+
+// function addingEventListener() {
+//     alert("I've been clicked")
+// }
+
+
+// function addingEventListener() {
+//     alert("I've been clicked")
+    
+// }
+// document.querySelector("body").addEventListener("click", addingEventListener) 
+
+const input = document.getElementById('button');
+
+function addingEventListener(){
+    alert("I've been clicked");
+    input.addEventListener('click', addingEventListener);
 }
+
+
